d862ab18621d9761b78620fb931037c9c0e7be56
John Sibert <johnrsibert@gmail.com>
2021-02-14 14:14:52 -0800

    R compartment as random effect

---
+++ TMB/remSIR.R
@@ -0,0 +1,391 @@
+SIR_path = '/home/jsibert/Projects/SIR-Models/'
+fit_path = paste(SIR_path,'fits/remSIR/',sep='')
+dat_path = paste(SIR_path,'dat/',sep='')
+TMB_path = paste(SIR_path,'TMB/',sep='')
+source(paste(TMB_path,'SIR_read_dat.R',sep=''))
+#source(paste(TMB_path,'remSIR_fit_to_df.R',sep=''))
+require(TMB)
+require(gtools)
+#rm(fit)
+print('starting')
+
+# ------------------ support functions ------------
+
+make.ytext = function(ylim,prop)
+{
+    ytext = prop*(ylim[2]-ylim[1])+ylim[1]
+    return(ytext)
+}
+
+get.error = function(par,opt,map,tn)
+{
+    if ( is.na(map[tn]) )
+    {   
+        err = par[tn] 
+    }
+    else
+    {   
+        err = opt$par[tn] 
+    } 
+
+    print(paste('get.error',tn,err))
+    return(as.numeric(err))
+}
+
+plot.rv = function(x, obs_y, pred_y, ylab, err, err_name, ylim,pch='+', 
+                   obs_col='red', note_col='blue')
+{
+    plot(x, obs_y, ylab=ylab, ylim=ylim, pch=pch)
+    lines(x, pred_y, col=obs_col,lwd=3)
+    plot.error(x, pred_y, err)
+    ytext = make.ytext(ylim,0.9)
+#   print(paste('mean(err)',mean(err)))
+    print(paste(ylab,err_name,err))
+    if (err > 0.0)
+    {
+        note = paste(err_name,'~',sprintf("%.5g",err))
+    }
+    else
+    {
+        note = err_name
+    }
+    ttext = 0.1*(length(obs_y)-1)
+    text(ttext,ytext,note,col=note_col,pos=4)
+}
+
+plot.log.state = function(fit) #,np=5)
+{
+    attach(fit)
+
+    bias.note = function(name,err=NA)
+    {
+        w = which(names(obj$report()) == name)
+        val = as.numeric(obj$report()[w])
+        note = paste('bias = ',signif(val,3),sep='')
+        return(note)
+    }
+
+    fn = opt$value
+    if (is.null(fn))
+        fn = opt$objective
+    title =  paste(data$county,", f = ",sprintf('%.5g',fn),
+                   ", ",model.name,
+                   ", C = ", opt$convergence,
+                   " (",(opt$convergence==0),")",sep='')
+    width = 8.0
+    height = 8.0
+    x11(width=width,height=height,title=title)#,xpos=100)
+
+    old.par = par(no.readonly = TRUE) 
+    par(mar=c(2.0,4.0,0.5,0.5)+0.1) #,oma = c(5,4,0,0) + 0.1)#mfcol=c(3,2),
+ 
+    note.color='blue'
+    point.symb = '+'
+
+    lm = layout(matrix(c(1:8),ncol=2,byrow=FALSE))
+    layout.show(lm)
+#   mtext(title,outer=TRUE,side=1)
+
+
+    tt = seq(0,(length(data$log_obs_cases)-1))
+    ttext = 0.1*(length(data$log_obs_cases)-1)
+
+    poplim = c(0.0,1.2*log(data$N0)) #range(obj$report()$logS)
+
+    plot(tt,obj$report()$logS,ylab='ln S',ylim=poplim, pch=point.symb)
+#   err = SElogS
+#   logspace.plot.error(tt,obj$report()$logS,err)
+    gmlogS = median(obj$report()$logS)
+    abline(h=gmlogS,lty='dashed')
+
+
+    err = exp(get.error(par,opt,map,'logsigma_logC'))
+    plot.rv(tt,data$log_obs_cases, obj$report()$logEye,
+            ylab='ln cases,', err, err_name='sigma_logC',ylim=poplim)
+
+#   err = exp(get.error(par,opt,map,'logsigma_logR'))
+#   plot.rv(tt,data$log_obs_R, obj$report()$logR,
+#           ylab='ln R', err, err_name='sigma_logR',ylim=poplim)
+
+    plot(tt,obj$report()$logR,ylab='ln R',ylim=poplim, col='red',type='l',lwd=3)
+
+    ylim=c(0.0,1.2*max(data$log_obs_deaths,obj$report()$logD))
+    if (hasName(par,'logsigma_logD'))
+    {
+    #   Zero infltated log normal
+        err = exp(get.error(par,opt,map,'logsigma_logD'))
+        ename = 'sigma_logD'
+    }
+    else
+    {
+    #   Poisson error
+        err = 0.0
+        ename = 'lambda'
+    }
+    plot.rv(tt,data$log_obs_deaths, obj$report()$logD,
+            ylab='ln deaths', err, err_name=ename,ylim=ylim)
+
+    rlim = c(-10.0,1.0)
+    err = exp(get.error(par,opt,map,'logsigma_logbeta'))
+    plot.rv(tt,obj$report()$logbeta, obj$report()$logbeta,
+            ylab='ln beta', err=err,
+            err_name='sigma_logbeta',ylim=rlim)
+ 
+#   rlim = c(-10.0,1.0)
+#   err = exp(get.error(par,opt,map,'logsigma_logmu'))
+#   plot.rv(tt,obj$report()$logmu, obj$report()$logmu,
+#           ylab='ln mu', err=err,
+#           err_name='sigma_logmu',ylim=rlim)
+
+#   rlim = range(obj$report()$logZ)
+#   err = exp(get.error(par,opt,map,'logsigma_logZ'))
+#   plot.rv(tt,obj$report()$logZ, obj$report()$logZ,
+#           ylab='ln Z', err=err,
+#           err_name='sigma_logZ',ylim=rlim)
+
+
+    plot(tt,obj$report()$loggamma,ylab='ln gamma',ylim=rlim,  col='red',type='l',lwd=3)
+
+#   plot.new()
+#   mtext(title,outer=FALSE)
+
+#   print(dev.cur())
+#   dev.copy2pdf(file='ests.pdf',width=6.5,height=6.5)
+#   print(dev.cur())
+
+    par(old.par)
+    detach(fit)
+    return(dev.cur())
+}
+
+plot.error=function(x,y,sd,bcol='black',fcol='gray',mult=2)
+{
+   if (capabilities("cairo"))
+   {
+      sdyu = y+mult*sd
+      sdyl = y-mult*sd
+      frgb = col2rgb(fcol)/255
+      
+      polygon(c(x,rev(x)),c(sdyl,rev(sdyu)),
+              border=bcol,lty="dashed",lwd=1,
+              col=rgb(frgb[1],frgb[2],frgb[3],0.5))
+   }
+   else
+      polygon(c(x,rev(x)),c(sdyl,rev(sdyu)),
+              border=bcol,lty="dashed",lwd=1,col=fcol)
+
+}
+
+
+# ------------------ do_one run -------------------
+
+do_one_run = function(County = "Santa Clara",model.name = 'remSIR',do.plot=TRUE)
+{
+    cname = sub(" ","_",County)
+    datfile=paste(dat_path,cname,'.dat',sep='')
+    print(paste(separator,County,separator),quote=FALSE)
+    dat = read.dat.file(datfile, max_ntime = 100)
+    eps = 1e-8
+    data=dat$data
+    print(names(data))
+    print(data)
+    data$log_obs_cases = log(data$obs_cases+eps)
+    data$log_obs_deaths = log(data$obs_deaths+eps)
+    print("-data:")
+    print(data)
+    print(paste('cases:',length(data$obs_cases)))
+
+    init = list(
+    #   logsigma_logCP    = -0.5, #log(0.105),
+        logsigma_logRP    = log(0.223),
+    #   logsigma_logDP    = -5.0, #log(0.105),
+
+        logsigma_logbeta = log(0.223),
+        logsigma_loggamma =  log(0.223),
+    #   logsigma_logmu   = -0.5, #log(0.223),
+    
+    
+        logsigma_logC = log(0.223),
+        logsigma_logD = log(0.105),
+
+        logbeta  = -1.0, #log(0.01),
+        loggamma     = -2.0, #log(0.005),
+        logmu    = -5.0 #log(0.0001)
+    #   logR     =  data$log_obs_cases
+    )
+    print("--init parameter values:")
+    print(init)
+    
+    par = list(
+    #   logsigma_logCP = init$logsigma_logCP,
+        logsigma_logRP = init$logsigma_logRP,
+    #   logsigma_logDP = init$logsigma_logDP,
+    
+        logsigma_logbeta = init$logsigma_logbeta,
+        logsigma_loggamma = init$logsigma_loggamma,
+    #   logsigma_logmu = init$logsigma_logmu,
+    
+        logsigma_logC   = init$logsigma_logC,
+        logsigma_logD   = init$logsigma_logD,
+    
+        logbeta  = rep(init$logbeta,data$ntime+1),
+        loggamma = rep(init$loggamma,data$ntime+1),
+        logmu    = init$logmu
+    #   logR     = rep(init$logR,data$ntime+1) 
+    #   logR     = init$logR
+    ) 
+    print(paste("---initial model parameters: ", length(par)))
+    print(par)
+    
+    map = list(
+    #          "logsigma_logCP" = as.factor(1),
+               "logsigma_logRP" = as.factor(NA),
+    #          "logsigma_logDP" = as.factor(1),
+    
+    #          "logsigma_logbeta" = as.factor(1),
+               "logsigma_loggamma" = as.factor(NA),
+    #          "logsigma_logmu" = as.factor(1),
+    
+               "logsigma_logC" = as.factor(NA),
+               "logsigma_logD" = as.factor(NA)
+
+    #          "logbeta" = rep(as.factor(NA), data$ntime+1),
+    #          "loggamma"    = rep(as.factor(NA), data$ntime+1),
+    #          "logR"    = rep(as.factor(NA), data$ntime+1),
+    #          "logmu"   = as.factor(1)
+    )
+    
+    print(paste("---- estimation map:",length(map),"variables"))
+    print(map)
+    
+    cpp.name = paste(model.name,'.cpp',sep='')
+    
+    print(paste("Compiling",cpp.name,"-------------------------"),quote=FALSE)
+    compile(cpp.name)
+    
+    print(paste("Loading",model.name,"-------------------------"),quote=FALSE)
+    dyn.load(dynlib(model.name))
+    print("Finished compilation and dyn.load-------------",quote=FALSE)
+    print("Calling MakeADFun-----------------------------",quote=FALSE)
+    obj = MakeADFun(data,par,  random=c("loggamma","logbeta"), 
+                    map=map,DLL=model.name)
+    print("--------MakeADFun Finished--------------------",quote=FALSE)
+    print("obj$par (1):")
+    print(obj$par)
+    hide_obj = obj
+    lb <- obj$par*0-Inf
+    ub <- obj$par*0+Inf
+    
+    #   cmd = 'Rscript --verbose simpleSIR4.R'
+    
+    obj$env$inner.control$tol10 <- 0
+
+    nlminb.con=list(eval.max=5000,iter.max=5000)
+    
+    print("Starting minimization-------------------------",quote=FALSE)
+    opt = nlminb(obj$par,obj$fn,obj$gr,control=nlminb.con)
+    #opt = optim(obj$par,obj$fn,obj$gr)
+    
+    print("Done minimization-----------------------------",quote=FALSE)
+    print(paste("Function objective =",opt$objective))
+    print(paste("Function value =",opt$value))
+    print(paste("Convergence ",opt$convergence))
+    print(paste("Number of parameters = ",length(opt$par)),quote=FALSE)
+
+    fit = list(data=data,map=map,par=par,obj=obj,opt=opt,init=init,
+               model.name=model.name)
+    if (do.plot){
+    #   x11()
+    #   print(fit)
+        print('plotting')
+        plot.log.state(fit)
+        dev.file = paste(fit_path,data$county,'.pdf',sep='')
+    #   dev.file = paste(data$county,'_ests.pdf',sep='')
+        dev.copy2pdf(file=dev.file,width=6.5,height=9)
+    #   dev.off()
+    }
+    
+#   rd.file = paste(fit_path,data$county,'.RData',sep='')
+#   save.fit(fit,file=data$county,mod=model.name)#   rd.file) #"t.RData")
+    
+    return(fit)
+
+} # do_one_run = function(County = ...)
+
+
+separator = "#############################"
+print(separator)
+
+
+nrun = 1
+print(paste('nrun =',nrun))
+if (nrun < 2) {
+#   County="Miami-DadeFL"
+#   County="KingWA"
+    County="AlamedaCA"
+#   County = "Los_AngelesCA"
+#   County = "New_York_CityNY"
+    print(paste('----nrun =',nrun,County))
+    do_one_run(County=County)->fit
+    for (n in 1:length(fit$opt$par))
+    {
+    #   print(paste('name:',names(fit$opt$par[n])))
+    #   print(paste('     ',fit$opt$par[n]))
+        name = names(fit$opt$par[n])
+        w = which(names(fit$par)==name)
+    #   print(paste(name,w))
+        print(paste(name,fit$opt$par[n],fit$par[w]))
+    }
+#   print(paste('median logbeta',median(fit$obj$report()$logbeta)))
+#   print(paste('median loggamma',median(fit$obj$report()$loggamma)))
+    print(paste('logmu',fit$obj$par['logmu']))
+} else {
+    print(paste('----nrun =',nrun))
+    sink( paste(fit_path,'SIR_model.log',sep=''), type = c("output", "message"))
+    dp =paste(dat_path,'*.dat',sep='')
+    print(dp)
+#   print(paste('globbing',dp))
+#   cc = Sys.glob(dp)
+cc = list(
+'AlamedaCA',
+'BexarTX',
+'BrowardFL',
+'ClarkNV',
+'CookIL',
+'DallasTX',
+'FranklinOH',
+'HarrisTX',
+'HennepinMN',
+'HillsboroughFL',
+"Los_AngelesCA", 
+"MaricopaAZ", 
+"Miami-DadeFL", 
+"MiddlesexMA", 
+"NassauNY", 
+"New_York_CityNY", 
+"OrangeCA", 
+"OrangeFL", 
+"Palm_BeachFL", 
+"PhiladelphiaPA", 
+"RiversideCA", 
+"SacramentoCA", 
+"San_BernardinoCA", 
+"San_DiegoCA", 
+"Santa_ClaraCA", 
+"SuffolkNY", 
+"TarrantTX", 
+"TravisTX", 
+"WayneMI") 
+
+    for (c in cc)
+    {
+        County = sub("\\.dat","",c)
+        c = basename(County)
+        print(paste("starting",c))
+        do_one_run(County=c,do.plot=FALSE)
+    }
+    sink()
+ }
+
+
+#'KingWA',
---
+++ TMB/remSIR.cpp
@@ -0,0 +1,260 @@
+#include <TMB.hpp>
+#include <math.h>
+#include <fenv.h> 
+#include "trace.h"
+
+const double TWO_M_PI = 2.0*M_PI;
+const double eps = 1e-8;
+const double logeps = log(eps);
+
+template < class Type > Type square(Type x)
+{
+    return x * x;
+}
+
+// log-normal error
+template <class Type>
+Type LNerr(Type logobs, Type logpred, Type var)
+{
+    Type nll = 0.5*(log(TWO_M_PI*var) + square(logobs-logpred)/var);
+    // identical:
+    //Type tnll = -dnorm(logobs,logpred,sqrt(var),true);
+    //TTRACE(nll,tnll)
+    return nll;
+}
+
+// zero-inflated log-normal error
+template <class Type>
+Type ZILNerr(Type logobs, Type logpred, Type var, Type prop0 = 0.15)
+{
+    Type nll;
+
+    if (logobs > logeps)  //log zero deaths
+    {
+        nll = (1.0-prop0)*0.5*(log(TWO_M_PI*var) + square(logobs - logpred)/var);
+    }
+    else
+    {
+        nll = prop0*0.5*(log(TWO_M_PI*var));
+    }
+    return nll;
+}
+
+template < class Type > Type isNaN(Type x, const int line)
+{
+    if (x != x)
+    {
+         TRACE(x)
+         std::cerr << "NaN at line " << line << std::endl;
+       //exit(1);
+    }
+    return x;
+}
+
+template<class Type> 
+Type objective_function <Type>::operator()()
+{
+// feenableexcept(FE_INVALID | FE_OVERFLOW | FE_DIVBYZERO | FE_UNDERFLOW);
+
+    DATA_SCALAR(N0)
+    DATA_INTEGER(ntime)
+    DATA_VECTOR(obs_cases)
+    DATA_VECTOR(obs_deaths)
+    DATA_SCALAR(prop_zero_deaths)
+    DATA_VECTOR(log_obs_cases)
+    DATA_VECTOR(log_obs_deaths)
+                  
+    //PARAMETER(logsigma_logCP);          // SIR cases process error
+    PARAMETER(logsigma_logRP);          // SIR removals process error
+    //PARAMETER(logsigma_logDP);          // SIR deaths process error
+
+    PARAMETER(logsigma_logbeta);       // beta random walk sd
+    PARAMETER(logsigma_loggamma);      // gamma randomwalk sd
+    //PARAMETER(logsigma_logmu);         // mu randomwalk sd
+
+    PARAMETER(logsigma_logC);          // cases observation error
+    PARAMETER(logsigma_logD);          // deaths observation error
+ 
+    PARAMETER_VECTOR(logbeta);         // infection rate time series
+    PARAMETER_VECTOR(loggamma);        // infected removal rate time series
+    PARAMETER(logmu);                  // mortality rate
+    //PARAMETER_VECTOR(logR);            // number of Recovered
+
+    // state variables
+    vector <Type> logS(ntime+1);      // number of Suscectibles
+    vector <Type> logEye(ntime+1);    // number of Infections
+    vector <Type> logR(ntime+1);      // number of Recovered
+    vector <Type> logD(ntime+1);      // number of Deaths from infected populationtime+1);
+    vector <Type> rho(ntime+1);       // basic reproduction ratio
+
+    Type sigma_logbeta = exp(logsigma_logbeta); 
+    Type sigma_loggamma = exp(logsigma_loggamma); 
+    //Type sigma_logmu = exp(logsigma_logmu); 
+    Type var_logbeta = square(sigma_logbeta);
+    Type var_loggamma = square(sigma_loggamma);
+    //Type var_logmu = square(sigma_logmu);
+
+    //Type sigma_logCP = exp(logsigma_logCP);
+    Type sigma_logRP = exp(logsigma_logRP);
+    //Type sigma_logDP = exp(logsigma_logDP);
+    //Type var_logCP = square(sigma_logCP);
+    Type var_logRP = square(sigma_logRP);
+    //Type var_logDP = square(sigma_logDP);
+
+    Type sigma_logC = exp(logsigma_logC);
+    Type sigma_logD = exp(logsigma_logD);
+    Type var_logC = square(sigma_logC);
+    Type var_logD = square(sigma_logD);
+
+    Type f = 0.0;
+    Type betanll = 0.0;
+    Type gammanll = 0.0;
+    //Type munll = 0.0;
+    Type Pnll = 0.0;
+    Type cnll = 0.0;
+    Type dnll = 0.0;
+
+    //  loop over time
+    logS[0] = log(N0);
+//  logEye[0] = log_obs_cases[0];
+    logR[0] = logeps;
+//  logD[0] = log_obs_deaths[0];
+    Type mu   = exp(logmu);
+    for (int t = 1; t <= ntime; t++)
+    {
+       //TRACE(t)
+
+       // infection rate random walk
+       //TRACE(t,var_logbeta)
+       //TTRACE(logbeta(t-1),logbeta(t))
+         betanll += isNaN(LNerr(logbeta(t-1),logbeta(t),var_logbeta),__LINE__);
+
+         // recovery rate random walk
+         gammanll += isNaN(LNerr(loggamma(t-1),loggamma(t),var_loggamma),__LINE__);
+ 
+         // mortality rate random walk
+         //munll += isNaN(LNerr(logmu(t-1),logmu(t),var_logmu),__LINE__);
+
+         // compute process error likelihoods
+         Type beta = exp(logbeta(t-1));
+         Type gamma = exp(loggamma(t-1));
+        
+         Type S   = exp(logS(t-1));
+         Type Eye = exp(logEye(t-1));
+         Type R   = exp(logR(t-1));
+         Type D   = exp(logD(t-1));
+         Type N   = S + Eye + R;
+         Type bison = beta * Eye * S/N;
+       //TTRACE(t,N)
+       //TTRACE(Eye,D)
+       //TTRACE(S,R)
+       //TRACE(bison)
+         
+         Type deltaS = -bison;
+         Type nextS = S + deltaS;
+         logS(t) = log(nextS);
+
+
+         Type deltaEye = bison - (gamma+mu)*Eye;
+         Type nextEye = Eye + deltaEye;
+         if (nextEye > 0.0)
+         {
+             logEye(t) = log(nextEye);
+         }
+         else
+         {
+             Pnll += square(deltaEye);
+             TTRACE(nextEye,deltaEye)
+         }
+         
+         // recovery rate process error
+         Type deltaR = gamma*Eye;
+         Type nextR = R + deltaR;
+         if (nextR > 0.0)
+         {
+             logR(t) = log(nextR);
+             Pnll += isNaN(LNerr(logR(t-1), logR(t),var_logRP),__LINE__);
+         }
+         else
+         {
+             Pnll += square(deltaR);
+             TTRACE(nextR,deltaR)
+         }
+
+         // deaths process error
+         Type deltaD = mu*Eye;
+         Type nextD = D + deltaD;
+         if (nextD > 0.0)
+         {
+             logD(t) = log(nextD);
+         }
+         else
+         {
+             Pnll += square(deltaD);
+             TTRACE(nextD,deltaD)
+         }
+
+     }
+
+     // compute observation likelihoods
+     for (int t = 0; t <= ntime; t++)
+     {   
+       //TRACE(t)
+         cnll += isNaN(  LNerr(log_obs_cases(t),logEye(t),var_logC),__LINE__);
+       //TTRACE(cnll,logEye(t))
+
+
+     //  Zero inflated log normal
+         dnll += isNaN(ZILNerr(log_obs_deaths(t),logD(t),var_logD, prop_zero_deaths),__LINE__);
+     //  dnll += isNaN(  LNerr(log_obs_deaths(t),logD(t),var_logD),__LINE__);
+
+     //  Poisson error
+     //  dnll += -isNaN(obs_deaths(t)*logD(t) - exp(logD(t)) - lfactorial(obs_deaths(t)),__LINE__);
+
+       //TTRACE(dnll,logD(t))
+
+     //  TTRACE(cnll,dnll)
+     }
+//   TTRACE(cnll,dnll)
+
+
+     // total likelihood
+     f += isNaN((betanll + gammanll + Pnll + cnll + dnll),__LINE__);
+
+     rho = exp(logbeta-loggamma);
+
+     REPORT(logS)
+     REPORT(logEye)
+     REPORT(logR)
+     REPORT(logD)
+
+     REPORT(logbeta)
+     REPORT(loggamma)
+     REPORT(logmu)
+     REPORT(rho)
+
+     //REPORT(sigma_logCP);
+     REPORT(sigma_logRP);
+     //REPORT(sigma_logDP);
+
+     REPORT(sigma_loggamma);
+
+     REPORT(f);
+     REPORT(betanll);
+     REPORT(Pnll);
+     REPORT(cnll);
+     REPORT(dnll);
+/*
+     if (1)
+     {
+     //   TRACE(logbeta)
+     //   TRACE(loggamma)
+          TRACE(betanll);
+          TRACE(gammanll);
+          TTRACE(cnll,dnll)
+     //   return(Type(-1.0));
+     }
+*/
+   //TRACE(f)
+     return isNaN(f,__LINE__);
+}
