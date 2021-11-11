87f905f5d5fc7069924a8405e96af2e20c5e5d17
John Sibert <johnrsibert@gmail.com>
2021-11-10 16:14:34 -0800

    runs for many date and counties; tight layout issue

--- python/CFR_trends.py
+++ python/CFR_trends.py
@@ -22,19 +22,14 @@ https://stackoverflow.com/questions/8747761/scipy-lognormal-distribution-paramet
 def CFR_trends(CFRfile = 'CFR.csv'):
 
     with open(CFRfile,'r') as rr:
-        print('Reading',CFRfile)
-        
-    #    window = None
-    #   nG,window,ndate = (map(int, rr.readline().split()))
-    #    window = map(int, rr.readline().split())
-    #    wl = rr.readline()
-    #    print(wl)
-    #    window = int(wl)
-    #   this only works if there is not 'comment' line at head of thhe file
-        window = int(rr.readline())
-         
+        print('Reading',CFRfile)  
+        line = '#'
+        while line[0] == '#':
+            line = rr.readline()
+       
+        window = int(line)
         print('window: ',window)
-        CFR = pd.read_csv(rr,header=1,index_col=0,comment='#')
+        CFR = pd.read_csv(rr,header=0,index_col=0,comment='#')
             
     print('finished reading',CFRfile,CFR.shape)
     
@@ -81,6 +76,7 @@ def CFR_trends(CFRfile = 'CFR.csv'):
         fails = 0
         param = pd.DataFrame(None,
                              columns=['shape','loc','scale','rmax'],
+                                     # 'm','v','s','k'],
                              index=CFR.index)
         for d in CFR.index:
         #    print(d)
@@ -88,14 +84,26 @@ def CFR_trends(CFRfile = 'CFR.csv'):
             mask = row > 0.0
         #    print(mask)
         #    print(row)
-        
+     
         #   lnparam      = stats.lognorm.fit(dat,floc=0)
             try:
                 param.loc[d][0:3] = stats.lognorm.fit(row[mask].dropna(),
                                                  floc=0)
-            
+                #try:
+                #    ss = pd.Series(stats.lognorm.stats(param.loc[d][0],param.loc[d][1],param.loc[d][2],
+                #                               moments='mvsk'))
+                #    print(ss)
+                #    for i in range(0,4):
+                #        param.loc[d][4+i] = ss[i]
+                    
+                #except Exception as e: 
+                #    print('stats exception',e,' in row',row)
+                #    print(param.loc[d])
+       
+                            
             except:
-                print('fit exception in row',d,row)
+                fails += 1
+                #print('fit exception in row',d,row)
                 #print(param.loc[d])
                 param.loc[d] = param.loc[d].fillna(1e-5)
                 #print(param.loc[d])
@@ -103,11 +111,15 @@ def CFR_trends(CFRfile = 'CFR.csv'):
                 
        
     #    param.filna(0.0)   
-    #    print(param)
-        matplotlib.rcParams.update({"figure.autolayout": False})
+        
+        print('stats lognormal fit fialed for',fails,' dates')
+        #print(param)
+        
         fig, ax = plt.subplots(2,2,figsize=(11.0,6.5))
         Y = np.linspace(0.0,0.06,100)
- 
+   #     matplotlib.rcParams.update({"figure.autolayout": False})
+        plt.rcParams["figure.autolayout"] = False
+  
         colors = plt.cm.Blues_r(np.linspace(0,1,ndate))
    #     colors = plt.cm.viridis(np.linspace(0,1,ndate))
         mdate = mdates.date2num(param.index)
@@ -117,7 +129,7 @@ def CFR_trends(CFRfile = 'CFR.csv'):
             pdf = pd.Series(stats.lognorm.pdf(Y,p['shape'],p['loc'],p['scale']))
         #    cdf = pd.Series(stats.lognorm.cdf(Y,p['shape'],p['loc'],p['scale']))
             pdf = pdf/pdf.sum()
-        
+         
         #   find mode, the value of Y where pdf is maximum
             rmax = 0.0
             for i in range(0,len(pdf)-1):
@@ -127,44 +139,55 @@ def CFR_trends(CFRfile = 'CFR.csv'):
                 #    print('rmax =',rmax, 'at ',i)
                     break
             
-            ax[0,0].plot(Y,pdf,linewidth=2,color=colors[d])
+            ax[0,0].plot(Y,pdf,linewidth=1,color=colors[d])
             ax[0,0].set_ylabel('P(r)')
             ax[0,0].set_xlabel('Deaths/Cases (r)')
-
-    #    print(param)
+        
+        ax[0,0].set_ylim(0.0,0.15)
+   
+        print(param)
             
-        GU.make_date_axis(ax[1,0])
-    #    ax[1,0].set_ylabel('central tendencies)')
-        '''
-df = pd.DataFrame({'one': range(10), 'two': range(10, 20)})
-
-ax = df['one'].plot()
-ax2 = df['two'].plot(secondary_y=True)
-        '''
+     
    #     ax2 = ax[1,0].twinx()
    #     ax2.plot(mdate,np.log(param['scale']))
    #     GU.mark_ends(ax2, mdate,np.log(param['scale']) ,'mu')
    #     ax[1,0].plot(mdate,param['scale'])
    
-        ax[0,0].set_ylim(0.0,0.15)
-   
+        
+        GU.make_date_axis(ax[1,0])
         ax[1,0].plot(mdate,param['scale'])
-        GU.mark_ends(ax[1,0], mdate,param['scale'] ,'mu','r')
+        GU.mark_ends(ax[1,0], mdate,param['scale'] ,'mu','b')
         ax[1,0].set_ylabel('Central Tendency')
-   #    ax[1,0].plot((0,0),(0,0),linewidth=0.5)
         ax[1,0].plot(mdate,param['rmax'])
-        GU.mark_ends(ax[1,0], mdate,param['rmax'] ,'mode','r')
+        GU.mark_ends(ax[1,0], mdate,param['rmax'] ,'mode','b')
         ax[1,0].set_ylim(0.0,0.06)
+        #ax[1,0].plot(mdate,param['m'],linewidth=1)
+        #GU.mark_ends(ax[1,0], mdate,param['m'] ,'m','r')
+        #ax[1,0].set_ylim(0.0,0.06)
             
         GU.make_date_axis(ax[0,1])
         ax[0,1].plot(mdate,param['shape'])
         ax[0,1].set_ylabel('sigma = shape')
+        #sv = param['v'].pow(0.5)
+        #ax[0,1].plot(mdate,sv,linewidth=1)
+        #GU.mark_ends(ax[1,0], mdate, sv,'s','r')
             
         GU.make_date_axis(ax[1,1])
         ax[1,1].plot(mdate,param['scale'])
         ax[1,1].set_ylabel('exp(mu) = scale')
         ax[1,1].set_ylim(0.0,0.06)
-    
+           
+        #ax[1,1].plot(mdate,param['k'],linewidth=1)
+        #GU.mark_ends(ax[1,1], mdate,param['k'] ,'k','r')
+        
+                                      
+        title = 'CFR properties for {0:d} counties, {1:d} time periods, \
+                 averaged over a {2:d} day moving window\n'.format(nG,ndate,window)
+        #line = '{0: 7d}{1: 5d}{2: 7d}\n'.format(ndate,nG,w)
+        #title = 'Title'
+        print(title)
+ 
+        #fig.suptitle(title,size='medium')
         plt.show()
         
         gfile = 'CFRtrend_'+str(ndate)+'_'+str(nG)+'_'+str(window)+'.png'
