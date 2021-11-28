4c0518f2a8bdcaafcf904f5ac14baf85f6415775
John Sibert <johnrsibert@gmail.com>
2021-11-28 11:07:15 -0800

    clean up and test

--- python/vax.py
+++ python/vax.py
@@ -17,7 +17,7 @@ from covid21 import GraphicsUtilities as GU
 
 #rom numpy import errstate,isneginf #,array
 import pandas as pd
-from datetime import date, datetime, timedelta
+#from datetime import date, datetime, timedelta
 #from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -25,7 +25,7 @@ import matplotlib.dates as mdates
 #from matplotlib import rc
 import numpy as np
 #import os
-import sys
+#import sys
 #import pyreadr
 #from io import StringIO 
 #from io import BytesIO
@@ -43,21 +43,13 @@ import sys
 
 # wget https://data.cdc.gov/api/views/8xkx-amqh/rows.csv
 
-def read_vax():
+def NOT_read_vax():
     vax_file = '~/Downloads/COVID-19_Vaccinations_in_the_United_States_County.csv'
     print('reading',vax_file)
     tvax = pd.read_csv(vax_file,header=0)
     print(tvax)
     
-    '''
-    pdate = [0.0]*len(tvax)
-    c = 0
-    for k,d in enumerate(tvax['Date']):
-        c += 1
-        pdate[k] = mdates.date2num(datetime.strptime(d,'%m/%d/%Y').date())
-        if k <= 1:
-            print(c,k,d,pdate[k])
-    '''
+   
     pdate = pd.to_datetime(tvax['Date'],format ='%m/%d/%Y' )
 #    dat['fips'] = dat['fips'].fillna(0).astype(np.int64)
     tvax['FIPS'].replace('UNK',np.nan,inplace = True)# = tvax['FIPS'].fillna(0).astype(np.int64)
@@ -70,20 +62,8 @@ def read_vax():
     print(tvax.columns)
 
     vax=pd.DataFrame()#columns=['date','firstjab','full'])
-    '''
-     elf.pdate = []
-            for d in self.date:
-                self.pdate.append(mdates.date2num(datetime.strptime(d,'%Y-%m-%d').date()))
-            return(self.pdate)
-    print(vax)
-    pdate = [0.0]*len(tvax)
-    for k,d in enumerate(tvax['Date']):
-    #    print(k,d)
-        pdate[k] = mdates.date2num(datetime.strptime(d,'%m/%d/%Y').date())
-    '''  
     
     vax['date'] = pdate
-#   vax['Date'] = tvax['Date']
     vax['county'] = tvax['Recip_County']
     vax['code'] = tvax['Recip_State']
     vax['fips'] = tvax['FIPS']
@@ -98,82 +78,8 @@ def read_vax():
     vax.to_csv('vax.csv',header=True,index=False)
     print('saved vax.csv')
     
-def NOT_plot_vax():
-    vax = pd.read_csv('vax.csv')   
-    print(vax)
-    
-    fig, ax = plt.subplots(3,1,figsize=(6.5,9.0))
- #   firstDate = datetime.strptime(vax['date'][0],'%Y-%m-%d')
-    print(vax['date'][0]) #,firstDate)
-    for a in ax:
-        GU.make_date_axis(a)#,cv.FirstNYTDate)#,firstDate)
-        
-        
- #  Los Angeles
-    pop = 10039107.0
-    mult = 100.0
-       
-    cmask = vax['fips'] == 6037
-    print(cmask)
-    print(vax[cmask])
-    mdate = pd.Series(mdates.date2num(vax['date'][cmask]))
-    print(mdate)
-    #print(vax[cmask].index[0])#vax[cmask].index)
-    print('mdate',len(mdate),mdate.min(),mdate.max())
-      
-    ax[0].plot(mdate,mult*vax['first'][cmask]/pop,linewidth=2)
-    GU.mark_ends(ax[0],mdate, mult*vax['first'][cmask]/pop, 'One','r')
-    ax[0].plot(mdate,mult*vax['full'][cmask]/pop,linewidth=2)
-    GU.mark_ends(ax[0],mdate,mult*vax['full'][cmask]/pop,' Full','r')
-    ax[0].set_ylabel('Vaccination rate (%)')
-    
-    #delta = np.diff(gdf.iloc[:,a])
-    dfirst = pd.Series(np.diff(vax['first'][cmask]))
-    ax[1].plot(mdate[1:],dfirst,linewidth=2)
-    GU.mark_ends(ax[1],mdate[1:],dfirst, 'One','r')
-    
-    dfull = pd.Series(np.diff(vax['full'][cmask]))
-    ax[1].plot(mdate[1:],dfull,linewidth=1)
-    GU.mark_ends(ax[1],mdate[1:],dfull, ' Full','r')
-    ax[1].set_ylabel('Vaccinations per day')
-    
-    tgeog = GG.Geography(name='Los Angeles',enclosed_by='California',code='CA')
-    tgeog.read_nyt_data('county')
-    #tgeog.print_metadata()
-    #tgeog.print_data()
-    #tgeog.plot_prevalence(save=True, signature=True,show_superspreader=False,per_capita=True,show_order_date = True)
-    #tgeog.plot_prevalence(save=False, signature=True,show_superspreader=False,
-    #        per_capita=True,show_order_date = True,cumulative = True)
-    #print(tgeog.date)
-    #print(tgeog.cases)
-    gdf = tgeog.to_DataFrame()
-    print(gdf)
-    
-    ax[2].plot(gdf.pdate,gdf.cases,linewidth=2)
-    GU.mark_ends(ax[2],gdf.pdate,gdf.cases,'C','r')
-    ax[2].set_ylabel('Cases')
-    ax[2].set_ylim(0.0,1.1*gdf.cases.max())
-    
-    ax2 = ax[2].twinx()
-    ax2.plot(gdf.pdate,gdf.deaths,linewidth=2,c='r')
-    GU.mark_ends(ax2,gdf.pdate,gdf.deaths,'D','r')
-    ax2.set_ylabel('Deaths')
-    #ax[1].set.ylim(0,1.5e6)
-    ax2.set_ylim(0.0,0.02*ax[2].get_ylim()[1])
-    #ax2_lim = ax[2].get_ylim()
-    #print(ax2_lim)
-    #ax2.set_ylim(ax2_lim[0], 0.02*ax2_lim[1])
-    fig.show()
-    
+   
 def get_cdc_dat(update=False):
-    '''
-    vax_file = '~/Downloads/COVID-19_Vaccinations_in_the_United_States_County.csv'
-    print('reading',vax_file)
-    tvax = pd.read_csv(vax_file,header=0)
-    print(tvax)
-    print(tvax.columns)
-    #[1119194 rows x 32 columns
-    '''
     vax_file = cv.CDC_home + 'us-vax.csv'
    
     if update:
@@ -262,57 +168,6 @@ def get_cdc_dat(update=False):
     vax.to_csv(vax_name,header=True,index=False)
     print('Augmented vax data written to',vax_name)
     
-    '''
-    NYC_data = pd.DataFrame(columns=['date','fips','county','code','first','full'])
-    NYC_data['date'] = udates
-    #NYC_data['fips'] = 36999
-    NYC_data['county'] = 'New York City'
-    NYC_data['code'] = 'NY'
-    NYC_data['fips'] = 36999
-    NYC_data['first'] = 0.0
-    NYC_data['full'] = 0.0
-    print(NYC_data)
-   
-    NYC_fips = [36005,36047,36061,36081,36085]
-    
-    for k,d in enumerate(udates):
-    #    print(k,d)
-        udmask = raw_data['date'] == d
-        for f in NYC_fips:
-            fipsmask = raw_data['fips'] == f
-            gmask = udmask & fipsmask
-                   
-            if gmask.value_counts()[1] <= 1:
-                NYC_data.loc[k,'first'] += float(raw_data.loc[gmask,'administered_dose1_recip'])
-                NYC_data.loc[k,'full'] += float(raw_data.loc[gmask,'series_complete_yes'])
-            #    print(raw_data.loc[gmask,['fips']])
-            else:
-                print('duplicate (date,fips) combination at',(d,f))
-                print('   ',True,'values in gmask =',gmask.value_counts()[1])
-                dupes = raw_data[gmask]
-                print('    duplicates:',dupes.index)
-                d0 = dupes.index[0]
-      
-                try:
-                    NYC_data.loc[k,'first'] += float(dupes.loc[d0,'administered_dose1_recip'])
-                    NYC_data.loc[k,'full']  += float(raw_data.loc[d0,'series_complete_yes'])
-                except Exception as exception:
-                    print('NYC_data assignment filed for',(d,f,d0))
-                    print('    Exception:',exception.__class__.__name__)
-                    print(dupes.loc[d0])
-                        
-    print(NYC_data)
-     
-    vax=pd.DataFrame()
-    vax['date'] = raw_data['date']
-    vax['county'] = raw_data['recip_county']
-    vax['code'] = raw_data['recip_state']
-    vax['fips'] = raw_data['fips']
-    vax['first'] = raw_data['administered_dose1_recip']
-    vax['full'] = raw_data['series_complete_yes']
-
-    print(vax)
-    '''
      
 def plot_vax(name='New York City',enclosed_by='New York',code='NY'):
 
@@ -321,13 +176,7 @@ def plot_vax(name='New York City',enclosed_by='New York',code='NY'):
     print('Vax data read from',vax_name)
         
     print(vax.tail())
-    
- #   fig, ax = plt.subplots(3,1,figsize=(6.5,9.0))
- #   firstDate = datetime.strptime(vax['date'][0],'%Y-%m-%d')
- #   print(vax['date'][0]) #,firstDate)
- #   for a in ax:
- #       GU.make_date_axis(a)#,cv.FirstNYTDate)#,firstDate)
-        
+          
     tgeog = GG.Geography(name=name,enclosed_by=enclosed_by,code=code)
     tgeog.read_nyt_data('county')
     tgeog.print_metadata()
@@ -338,7 +187,7 @@ def plot_vax(name='New York City',enclosed_by='New York',code='NY'):
     print(tgeog.moniker, fips, pop, mult)
     
     fmask = vax['fips'] == fips   
-    mdate = pd.Series(mdates.date2num(vax['date'][fmask]))
+    mdate = vax['mdate'][fmask]
     print(mdate)
     print(vax[fmask])
     pv = mult*pd.Series(vax['first'][fmask])/pop
@@ -358,5 +207,5 @@ def plot_vax(name='New York City',enclosed_by='New York',code='NY'):
     
 #read_NYC_data()
 #plot_NYC_data()
-#get_cdc_dat()#True)
+#get_cdc_dat(True)
 #plot_vax(name='Alameda',enclosed_by='California',code='CA')
