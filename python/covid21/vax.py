2654dcd8a26d47247ed29270001d9fae568c17b2
John Sibert <jsibert@gmail.com>
2022-06-06 09:36:15 -0700

    simplified version of get_cdc_dat() for testing

--- python/covid21/vax.py
+++ python/covid21/vax.py
@@ -25,7 +25,7 @@ import matplotlib.dates as mdates
 #from matplotlib import rc
 import numpy as np
 #import os
-#import sys
+import sys
 #import pyreadr
 #from io import StringIO 
 #from io import BytesIO
@@ -45,14 +45,27 @@ import numpy as np
    
 def get_cdc_dat(update=False):
     vax_file = cv.CDC_home + 'us-vax.csv'
+    print('vax_file = ',vax_file)
    
     if update:
+        #                https://data.cdc.gov/resource/8xkx-amqh.json
         api_end_point = 'https://data.cdc.gov/resource/8xkx-amqh.json'   
+      #  query = api_end_point+'?$limit=2000000'
         query = api_end_point+'?$limit=2000000'
+        
+        print('query =',query)
         raw_data = pd.read_json(query,dtype=True)
-        print(raw_data)
-    #    print(raw_data.columns)
-    #    print(raw_data.dtypes)
+    #    raw_data = pd.read_json('https://data.cdc.gov/resource/8xkx-amqh.json')
+        
+   #    print('raw_data:')
+   #    print(raw_data)
+        raw_data.fillna(0.0,inplace=True)
+        print(raw_data.tail(n=50))
+  #      print('zero:',raw_data.iloc[0])
+        print('columns')
+        print(raw_data.columns)
+        print('dtypes')
+        print(raw_data.dtypes)
     
         raw_data.to_csv(vax_file,header=True,index=False)
         print('saved CDC data to',vax_file)
@@ -61,17 +74,23 @@ def get_cdc_dat(update=False):
     raw_data['fips'].replace('UNK',np.nan,inplace = True)# = tvax['FIPS'].fillna(0).astype(np.int64)
     raw_data['fips'] = raw_data['fips'].fillna(0).astype(np.int64)
     
-    print(raw_data)
-    print(raw_data.columns)
+#   print(raw_data)
+#   print(raw_data.columns)
     
     udates = pd.Series(raw_data['date'].unique())
-    print('udates:')
+    print('udates:',len(udates))
     print(udates)
     vax_len = len(raw_data)+len(udates)
     print(vax_len,len(raw_data),len(udates))
     
-    vax=pd.DataFrame(index=np.arange(0,vax_len),
-                     columns = ['date','mdate','county','code','fips','first','full'])
+    vax=pd.DataFrame(0,index=np.arange(0,vax_len),
+                     columns = ['date', 'mdate', 'county', 'code', 'fips', 'first', 'full'])
+
+#   vax.astype({'date':'str', 'county':'str', 'code': 'str', 'mdate': 'float64',
+#               'fips': 'int64',  'first': 'int64', 'full': 'int64'}).dtypes
+#   {'col1': 'int32'}
+#   print('init vax:')
+#   print(vax)
     vax['date'] = raw_data['date']
     vax['county'] = raw_data['recip_county']
     vax['code'] = raw_data['recip_state']
@@ -79,31 +98,62 @@ def get_cdc_dat(update=False):
     vax['first'] = raw_data['administered_dose1_recip']
     vax['full'] = raw_data['series_complete_yes']
 
-    print(vax)
+#   print('udate vax:')
+#   print(vax)
+#   if (1): sys.exit(1)
     
     NYC_fips = [36005,36047,36061,36081,36085]
+    print(NYC_fips)
     
     k = len(raw_data)-1
-    #for u,d in enumerate(udates):
+    print('k =',k)
     for d in udates:
         k += 1
-    #    print(k,d)
+#       print('k =',k,'d =',d)
         vax.loc[k,'date'] = d
         vax.loc[k,'fips'] = 36999
         vax.loc[k,'county'] = 'New York City'
         vax.loc[k,'code'] = 'NY'
         vax.loc[k,'first'] = 0.0
         vax.loc[k,'full'] = 0.0
-        
+#       print('vax',d,':')
+#       print(vax)
         udmask = raw_data['date'] == d
+    #   print(raw_data['fips'])
+    #   print(NYC_fips)
+        fipsmask = raw_data['fips'].isin(NYC_fips)
+    #   print(fipsmask)
+        gmask = udmask & fipsmask
+#       print('gmask:')
+    #   print(gmask)
+#       print(raw_data[gmask])
+        
+#       print(raw_data.loc[gmask,'series_complete_yes'].sum())
+#       print(raw_data.loc[gmask,'census2019_65pluspop'].sum())
+
+        vax.loc[k,'first'] = raw_data.loc[gmask,'administered_dose1_recip'].sum()
+        vax.loc[k,'full'] = raw_data.loc[gmask,'series_complete_yes'].sum()
+        
+        '''       
         for f in NYC_fips:
             fipsmask = raw_data['fips'] == f
+            print('f =',f,'k =',k,'fipsmask =',fipsmask,raw_data.loc[fipsmask])
             gmask = udmask & fipsmask
-                   
-            if gmask.value_counts()[1] <= 1:
+            print('gmask:')
+            print('    value_counts:',len(gmask.value_counts()))
+            print(gmask.value_counts())
+            print(gmask.sum())
+            print("raw_data.loc[gmask,'administered_dose1_recip']")
+            print(raw_data.loc[gmask,'administered_dose1_recip'])
+            vax.loc[k,'first'] += float(raw_data.loc[gmask,'administered_dose1_recip'])
+            vax.loc[k,'full'] += float(raw_data.loc[gmask,'series_complete_yes'])
+       #    if gmask.value_counts()[1] <= 1:
+            if (len(gmask.value_counts())) >= 2:
+                print(raw_data.loc[gmask,['fips']])
+                print('     k =',k)
+                print('    ', raw_data.loc[gmask,'administered_dose1_recip'])                
                 vax.loc[k,'first'] += float(raw_data.loc[gmask,'administered_dose1_recip'])
                 vax.loc[k,'full'] += float(raw_data.loc[gmask,'series_complete_yes'])
-            #    print(raw_data.loc[gmask,['fips']])
             else:
                 print('duplicate (date,fips) combination at',(d,f))
                 print('   ',True,'values in gmask =',gmask.value_counts()[1])
@@ -118,16 +168,27 @@ def get_cdc_dat(update=False):
                     print('vax assignment filed for',(d,f,d0))
                     print('    Exception:',exception.__class__.__name__)
                     print(dupes.loc[d0])
+        '''       
+    #   for f in NYC_fips:
+        print('end of NYC_fips loop')
 
-    
+#   for d in udates:
+    print('end of udates loop')
+
+#   print('vax 0:')
+#   print(vax)
     vax['mdate'] = pd.Series(mdates.date2num(vax['date']))
     vax['fips'] = vax['fips'].fillna(0).astype(np.int64)
-
-    print(vax)
+#   print('vax 1:')
+#   print(vax)
     
+#   vax = vax.astype({'date':'str', 'county':'str', 'code': 'str', 'mdate': 'float64',
+#               'fips': 'int64',  'first': 'int64', 'full': 'int64'}).dtypes
+
+#   {'col1': 'int32'}
     print('sorting by date')
     vax = vax.sort_values(by='date',ascending=True)
-    print(vax)
+#   print(vax)
     
     
     vax_name =  cv.CDC_home + 'vax.csv'
